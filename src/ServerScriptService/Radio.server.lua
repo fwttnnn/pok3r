83f494e1cc75d0cfb70953caef137b679547e8cf
fwttnnn <lolywk@tutanota.com>
2025-07-13 13:51:34 +0700

    chore: initial commit

---
+++ src/ServerScriptService/Radio.server.lua
@@ -0,0 +1,18 @@
+-- A radio that plays through all players.
+
+soundIds = {9119383684}
+cursor = 1
+
+sound = Instance.new("Sound")
+sound.Name = "Background"
+sound.Parent = workspace.Invisible.SFX
+
+sound.SoundId = "rbxassetid://" .. soundIds[cursor]
+sound:Play()
+
+sound.Ended:Connect(function(_soundId)
+    cursor = (cursor % #soundIds) + 1
+
+    sound.SoundId = "rbxassetid://" .. soundIds[cursor]
+    sound:Play()
+end)
---
+++ src/StarterPlayer/StarterCharacterScripts/Camera.client.lua
@@ -0,0 +1,19 @@
+-- NOTE: placeholder as an example, this is my previous work and hass nothing to do with Marchand.
+
+local character: Character = script.Parent
+local camera: Camera = game.workspace.CurrentCamera
+
+camera.CameraType = Enum.CameraType.Scriptable
+camera.FieldOfView = 50
+
+function cameraLookAt(from: Position, to: Position)
+    camera.CFrame = CFrame.lookAt(from, to)
+end
+
+character.Humanoid.Touched:Connect(function(hit, _limb)
+    local camFolder = game.workspace:WaitForChild("Invisible"):WaitForChild("Camera")
+    if hit.Name == "Hit@Lobby" then cameraLookAt(camFolder.Lobby.Cam.Position, camFolder.Lobby.Focus.Position) end
+    if hit.Name == "Hit@Left"  then cameraLookAt(camFolder.Left.Cam.Position, camFolder.Left.Focus.Position) end
+    if hit.Name == "Hit@Right" then cameraLookAt(camFolder.Right.Cam.Position, camFolder.Right.Focus.Position) end
+    if hit.Name == "Hit@Middle" then cameraLookAt(camFolder.Middle.Cam.Position, camFolder.Middle.Focus.Position) end
+end)
