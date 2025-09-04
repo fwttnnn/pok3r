6d894a54f5d98e9e16e96dda04ebf8d6a7f454e1
fwttnnn <lolywk@tutanota.com>
2025-09-04 18:15:29 +0700

    feat: timer auto act

--- src/ServerStorage/Modules/Match.lua
+++ src/ServerStorage/Modules/Match.lua
@@ -98,89 +98,92 @@ function Match:_TurnBetting()
         local _player = queue:Pop()
         if not _player.Active then continue end
 
+        local actionHandlers = {
+            BET = function(action)
+                local amount = action.Amount
+                if amount <= 0 then return false end
+                if _player.Chips < amount then return false end
+                if lastAction.Type == "BET" and amount <= lastAction.Amount then return false end
+
+                highestBet = amount
+                _player.Chips -= amount
+                self.Table.Pot += amount
+
+                if _player.Chips <= 0 then
+                    _player.Active = false
+                end
+
+                -- NOTE: if a player past the first player bets
+                -- then, we go around.
+                local playerIndex = self.Table:PlayerIndex(_player.Player)
+                if playerIndex > 1 then
+                    print("[debug] looping, rebuilding queue")
+                    queue = Queue.new()
+
+                    for i = playerIndex + 1, #self.Table.Players do
+                        local p = self.Table.Players[i]
+                        if not p.Active then continue end
+
+                        queue:Push(p)
+                    end
+
+                    for i = 1, playerIndex - 1 do
+                        local p = self.Table.Players[i]
+                        if not p.Active then continue end
+
+                        queue:Push(p)
+                    end
+                end
+
+                print("[bet] pot increased", _player.Chips, self.Table.Pot)
+                return true
+            end,
+
+            CALL = function(action)
+                -- if not (lastAction.Type == "BET" or lastAction.Type == "CALL") then return false end
+                if lastAction.Type ~= "BET" and lastAction.Type ~= "CALL" then return false end
+
+                local amount = math.min(_player.Chips, highestBet)
+                if amount <= 0 then return false end
+
+                action.Amount = amount
+                _player.Chips -= amount
+                self.Table.Pot += amount
+
+                if _player.Chips <= 0 then
+                    _player.Active = false
+                end
+
+                print("[call] pot increased", _player.Chips, self.Table.Pot)
+                return true
+            end,
+
+            CHECK = function(action)
+                if lastAction.Type == "BET" or lastAction.Type == "CALL" then return false end
+
+                print("[check] game continues", _player.Chips, self.Table.Pot)
+                return true
+            end,
+
+            FOLD = function(action)
+                _player.Active = false
+                print("[fold] player out", _player.Chips, self.Table.Pot)
+                return true
+            end,
+        }
+
         local acted = Instance.new("BindableEvent")
 
-        local connection
-        connection = Events.Act.OnServerEvent:Connect(function(player, action)
+        local actEventConnection
+        local timerFinishedConnection
+
+        actEventConnection = Events.Act.OnServerEvent:Connect(function(player, action)
             if player ~= _player.Player then return end
 
-            local handlers = {
-                BET = function()
-                    local amount = action.Amount
-                    if amount <= 0 then return false end
-                    if _player.Chips < amount then return false end
-                    if lastAction.Type == "BET" and amount <= lastAction.Amount then return false end
-
-                    highestBet = amount
-                    _player.Chips -= amount
-                    self.Table.Pot += amount
-
-                    if _player.Chips <= 0 then
-                        _player.Active = false
-                    end
-
-                    -- NOTE: if a player past the first player bets
-                    -- then, we go around.
-                    local playerIndex = self.Table:PlayerIndex(player)
-                    if playerIndex > 1 then
-                        print("[debug] looping, rebuilding queue")
-                        queue = Queue.new()
-
-                        for i = playerIndex + 1, #self.Table.Players do
-                            local p = self.Table.Players[i]
-                            if not p.Active then continue end
-
-                            queue:Push(p)
-                        end
-
-                        for i = 1, playerIndex - 1 do
-                            local p = self.Table.Players[i]
-                            if not p.Active then continue end
-
-                            queue:Push(p)
-                        end
-                    end
-
-                    print("[bet] pot increased", _player.Chips, self.Table.Pot)
-                    return true
-                end,
-
-                CALL = function()
-                    if not (lastAction.Type == "BET" or lastAction.Type == "CALL") then return false end
-
-                    local amount = math.min(_player.Chips, highestBet)
-                    if amount <= 0 then return false end
-
-                    action.Amount = amount
-                    _player.Chips -= amount
-                    self.Table.Pot += amount
-
-                    if _player.Chips <= 0 then
-                        _player.Active = false
-                    end
-
-                    print("[call] pot increased", _player.Chips, self.Table.Pot)
-                    return true
-                end,
-
-                CHECK = function()
-                    if lastAction.Type == "BET" or lastAction.Type == "CALL" then return false end
-
-                    print("[check] game continues", _player.Chips, self.Table.Pot)
-                    return true
-                end,
-
-                FOLD = function()
-                    _player.Active = false
-                    print("[fold] player out", _player.Chips, self.Table.Pot)
-                    return true
-                end,
-            }
-
-            local handler = handlers[action.Type]
+            local handler = actionHandlers[action.Type]
             if not handler then return end
 
-            local accepted = handler()
+            local accepted = handler(action)
             if not accepted then return end
 
             lastAction = {
@@ -188,23 +191,48 @@ function Match:_TurnBetting()
                 Amount = action.Amount,
             }
 
-            print("fired")
             acted:Fire()
             Events.Acted:FireClient(_player.Player)
 
-            connection:Disconnect()
+            actEventConnection:Disconnect()
+            timerFinishedConnection:Disconnect()
+        end)
+        
+        timerFinishedConnection = self.Timer.Finished:Connect(function()
+            print("[timeout] auto-act for " .. _player.Player.DisplayName)
+
+            local actions = {"CHECK", "CALL", "FOLD"}
+            for _, action in ipairs(actions) do
+                local fakeAction = { Type = action, Amount = nil }
+                local accepted = actionHandlers[action](fakeAction)
+
+                if accepted then 
+                    lastAction = fakeAction
+                    break
+                end
+            end
+
+            acted:Fire()
+            Events.Acted:FireClient(_player.Player)
+
+            actEventConnection:Disconnect()
+            timerFinishedConnection:Disconnect()
         end)
 
         Events.Act:FireClient(_player.Player)
         assert(not self.Timer.Running)
-        -- self.Timer:Reset()
-        -- self.Timer:Start(15)
+        self.Timer:Reset()
+        self.Timer:Start(5)
 
         print("waiting " .. _player.Player.DisplayName .. " to act")
         acted.Event:Wait()
         print("player: " .. _player.Player.DisplayName .. " acted")
-        -- self.Timer:Stop()
-        -- self.Timer:Reset()
+        self.Timer:Stop()
+        self.Timer:Reset()
+
+        actEventConnection:Disconnect()
+        timerFinishedConnection:Disconnect() -- NOTE: make sure timer is disconnected. 
+                                             -- this can still be called because of the Timer:Reset() if not done.
 
         self.Last = self.Table:PlayerIndex(_player.Player)
     end
@@ -231,7 +259,7 @@ function Match:_TurnShowdown()
     end)
 
     local winner = hands[1]
-    print("[end] match ended, winner: (" .. winner.Player.DisplayName .. ") :: " .. winner.Rank)
+    print("[end] match ended, winner: (" .. winner.Player.DisplayName .. ") :: " .. winner.Rank .. " @ " ..  winner.Score)
 
     for i = 2, #hands do
         print("[end] others: (" .. hands[i].Player.DisplayName .. ") :: " .. hands[i].Rank)
